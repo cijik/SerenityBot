ca3e42f35dbaf15447bbf3bd77770143ff63cc09
cijik <cijik1@gmail.com>
2023-03-12 13:49:19 +0200

    Dependency and Java upgrade

--- src/main/java/com/ciji/serenity/service/SerenityEventAdapter.java
+++ src/main/java/com/ciji/serenity/service/SerenityEventAdapter.java
@@ -3,7 +3,9 @@ package com.ciji.serenity.service;
 import com.ciji.serenity.enums.Commands;
 import com.ciji.serenity.exception.OptionNotFoundException;
 import discord4j.core.event.ReactiveEventAdapter;
-import discord4j.core.event.domain.InteractionCreateEvent;
+import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.event.domain.interaction.InteractionCreateEvent;
 import discord4j.discordjson.json.MessageData;
 import lombok.AllArgsConstructor;
 import org.reactivestreams.Publisher;
@@ -19,27 +21,54 @@ public class SerenityEventAdapter extends ReactiveEventAdapter {
     private final DateService dateService;
 
     @Override
-    public Publisher<?> onInteractionCreate(InteractionCreateEvent event) {
+    public Publisher<?> onApplicationCommandInteraction(ApplicationCommandInteractionEvent event) {
         try {
             switch (Commands.fromString(event.getCommandName())) {
-                case TODO: return doTodo(event);
-                case GET_TIMER: return timerService.getTime(event);
-                case START_TIMER: return timerService.startClock(event);
-                case STOP_TIMER: return timerService.stopClock(event);
-                case SET_DATE: return dateService.setDate(event);
-                case GET_DATE: return dateService.getDate(event);
-                case ADD_DAYS: return dateService.addDays(event);
+                case TODO -> {
+                    return doTodo(event);
+                }
+                case GET_TIMER -> {
+                    return timerService.getTime(event);
+                }
+                case START_TIMER -> {
+                    return timerService.startClock(event);
+                }
+                case STOP_TIMER -> {
+                    return timerService.stopClock(event);
+                }
             }
         } catch (OptionNotFoundException e) {
-            return event.acknowledge()
+            return event.reply()
                     .then(event.getInteractionResponse()
                             .createFollowupMessage("No option specified!"));
         }
         return Mono.empty();
     }
 
-    private Mono<MessageData> doTodo(InteractionCreateEvent event) {
-        return event.acknowledge()
+    @Override
+    public Publisher<?> onChatInputInteraction(ChatInputInteractionEvent event) {
+        try {
+            switch (Commands.fromString(event.getCommandName())) {
+                case SET_DATE -> {
+                    return dateService.setDate(event);
+                }
+                case GET_DATE -> {
+                    return dateService.getDate(event);
+                }
+                case ADD_DAYS -> {
+                    return dateService.addDays(event);
+                }
+            }
+        } catch (OptionNotFoundException e) {
+            return event.reply()
+                    .then(event.getInteractionResponse()
+                            .createFollowupMessage("No option specified!"));
+        }
+        return Mono.empty();
+    }
+
+    private Mono<MessageData> doTodo(ApplicationCommandInteractionEvent event) {
+        return event.reply()
                 .then(event.getInteractionResponse()
                         .createFollowupMessage("Things to do today:\n - write a bot\n - eat lunch\n - play a game"));
     }
--- src/main/java/com/ciji/serenity/service/TimerService.java
+++ src/main/java/com/ciji/serenity/service/TimerService.java
@@ -3,11 +3,10 @@ package com.ciji.serenity.service;
 import com.ciji.serenity.calendar.EquestrianClock;
 import com.ciji.serenity.calendar.EquestrianDate;
 import com.ciji.serenity.dao.BotParameterDao;
-import discord4j.core.event.domain.InteractionCreateEvent;
-import discord4j.core.object.entity.Message;
+import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent;
 import discord4j.discordjson.json.MessageData;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.time.StopWatch;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -16,25 +15,27 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@RequiredArgsConstructor
 public class TimerService {
 
-    @Autowired
-    private BotParameterDao botParameterDao;
+    private final BotParameterDao botParameterDao;
 
-    private StopWatch stopWatch;
+    private final StopWatch stopWatch = StopWatch.create();
 
     private final Timer timer = new Timer();
+
+    private boolean timerIsRunning = false;
 
     //TODO: add commands to resume the clock from DB
     private EquestrianClock clock;
 
-    public Mono<MessageData> getTime(InteractionCreateEvent event) {
+    public Mono<MessageData> getTime(ApplicationCommandInteractionEvent event) {
         //TODO: retrieve data from the clock, both the DB or current, maybe split into two methods
         try {
             long hours = stopWatch.getTime(TimeUnit.HOURS);
             long minutes = stopWatch.getTime(TimeUnit.MINUTES) % 60;
             long seconds = stopWatch.getTime(TimeUnit.SECONDS) % 60;
-            return event.acknowledge()
+            return event.reply()
                     .then(event.getInteractionResponse()
                             .createFollowupMessage("Current timer: " + hours + ":" + minutes + ":" + seconds));
         } catch (NullPointerException e) {
@@ -42,21 +43,31 @@ public class TimerService {
         }
     }
 
-    public Mono<MessageData> startClock(InteractionCreateEvent event) {
-        clock = new EquestrianClock(0);
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                clock.tick();
-                checkDayCounter();
-            }
-        }, 0, 1000);
-        return postMessage(event, "Clock started.");
+    public Mono<MessageData> startClock(ApplicationCommandInteractionEvent event) {
+        if (!timerIsRunning) {
+            clock = new EquestrianClock(0);
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    clock.tick();
+                    checkDayCounter();
+                }
+            }, 0, 1000);
+            timerIsRunning = true;
+            return postMessage(event, "Clock started.");
+        } else {
+            return postMessage(event, "Clock is already running.");
+        }
     }
 
-    public Mono<MessageData> stopClock(InteractionCreateEvent event) {
-        timer.cancel();
-        return postMessage(event, "Timer stopped.");
+    public Mono<MessageData> stopClock(ApplicationCommandInteractionEvent event) {
+        if (timerIsRunning) {
+            timer.cancel();
+            timerIsRunning = false;
+            return postMessage(event, "Clock stopped.");
+        } else {
+            return postMessage(event, "No clock is running at the moment.");
+        }
     }
 
     private void checkDayCounter() {
@@ -71,8 +82,8 @@ public class TimerService {
         }
     }
 
-    private Mono<MessageData> postMessage(InteractionCreateEvent event, String message) {
-        return event.acknowledge()
+    private Mono<MessageData> postMessage(ApplicationCommandInteractionEvent event, String message) {
+        return event.reply()
                 .then(event.getInteractionResponse()
                         .createFollowupMessage(message));
     }
