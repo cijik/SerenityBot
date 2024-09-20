540f7a39c1116ca339d44e3ee74e59e8fcd8f838
cijik <cijik1@gmail.com>
2024-09-20 15:43:07 +0300

    Fixed condition for new command init

--- src/main/java/com/ciji/serenity/config/CommandInitializer.java
+++ src/main/java/com/ciji/serenity/config/CommandInitializer.java
@@ -32,7 +32,7 @@ public class CommandInitializer {
         List<ApplicationCommandData> existingCommands = client.getClient().getRestClient().getApplicationService().getGlobalApplicationCommands(applicationId).collectList().block();
         if (existingCommands != null && //could be null
                 (existingCommands.removeIf(existingCommand -> Command.fromString(existingCommand.name()) == null) //check for any deprecated commands
-                        || !Arrays.stream(Command.values()).allMatch(command -> existingCommands.stream().anyMatch(existingCommand -> existingCommand.name().equals(command.getCommand()))))) { //check for any new commands
+                        || Arrays.stream(Command.values()).allMatch(command -> existingCommands.stream().anyMatch(existingCommand -> existingCommand.name().equals(command.getCommand()))))) { //check for any new commands
             restClient.getApplicationService().bulkOverwriteGlobalApplicationCommand(applicationId, existingCommands.stream().map(ApplicationCommandRequestMapper::map).toList()).subscribe();
             for (SerenityCommand command : commandList) {
                 command.register();
