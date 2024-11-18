aeb98a2c2a8d8a18795a6d8c4f93f5d40fe0a92f
cijik <cijik1@gmail.com>
2024-11-18 22:47:33 +0200

    Rest of the tests for character sheet service

--- src/test/java/com/ciji/serenity/service/CharacterSheetServiceTest.java
+++ src/test/java/com/ciji/serenity/service/CharacterSheetServiceTest.java
@@ -2,6 +2,8 @@ package com.ciji.serenity.service;
 
 import com.ciji.serenity.model.CharacterSheet;
 import com.ciji.serenity.repository.CharacterSheetRepository;
+import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
@@ -25,17 +27,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CharacterSheetServiceTest {
 
     @Mock
     private ChatInputInteractionEvent event;
+
+    @Mock
+    private ApplicationCommandInteractionOptionData data;
 
     private ApplicationCommandInteractionOption parameter;
 
@@ -55,7 +62,6 @@ class CharacterSheetServiceTest {
 
     @BeforeEach
     void setUp() {
-        ApplicationCommandInteractionOptionData data = mock(ApplicationCommandInteractionOptionData.class);
         gateway = mock(GatewayDiscordClient.class);
         parameter = new ApplicationCommandInteractionOption(
                 gateway,
@@ -72,18 +78,18 @@ class CharacterSheetServiceTest {
         characterSheet.setName("Character");
 
         when(gateway.getRestClient()).thenReturn(mock(DiscordClient.class));
-        when(event.getOption(eq("name"))).thenReturn(Optional.of(parameter));
         when(event.getInteraction()).thenReturn(interaction);
         when(interaction.getUser()).thenReturn(user);
         when(user.getId()).thenReturn(snowflake);
         when(snowflake.asString()).thenReturn("1");
-        when(data.value()).thenReturn(Possible.of("Character"));
-        when(data.type()).thenReturn(3);
-        when(characterSheetRepository.findByNameAndOwnerId("Character", "1")).thenReturn(characterSheet);
     }
 
     @Test
     void getCharacter() {
+        when(event.getOption(eq("name"))).thenReturn(Optional.of(parameter));
+        when(data.value()).thenReturn(Possible.of("Character"));
+        when(data.type()).thenReturn(3);
+        when(characterSheetRepository.findByNameAndOwnerId("Character", "1")).thenReturn(characterSheet);
         Message messageResponse = new Message(gateway, MessageData.builder()
                 .applicationId(1L)
                 .id(1L)
@@ -111,21 +117,189 @@ class CharacterSheetServiceTest {
 
     @Test
     void getAllCharacters() {
+        Message messageResponse = new Message(gateway, MessageData.builder()
+                .applicationId(1L)
+                .id(1L)
+                .channelId(200L)
+                .type(ApplicationCommandOption.Type.STRING.ordinal())
+                .author(UserData.builder().id(1L).username("user").discriminator("#0").build())
+                .components(ComponentData.builder().type(MessageComponent.Type.BUTTON.ordinal()).build())
+                .content("Here's the list of all characters you own:\n**Character**")
+                .timestamp("today")
+                .tts(false)
+                .mentionEveryone(false)
+                .pinned(false)
+                .build());
+        when(event.createFollowup(any(InteractionFollowupCreateSpec.class)))
+                .thenReturn(Mono.just(messageResponse));
+        when(characterSheetRepository.findAllByOwnerId("1")).thenReturn(List.of(characterSheet));
+        when(event.createFollowup(anyString())).thenReturn(InteractionFollowupCreateMono.of(event).withContent(messageResponse.getContent()));
+        Mono<Message> response = characterSheetService.getAllCharacters(event);
+
+        StepVerifier.create(response)
+                .expectNextMatches(message -> message.getContent().equals(messageResponse.getContent()))
+                .verifyComplete();
     }
 
     @Test
     void addCharacter() {
+        ApplicationCommandInteractionOptionData data2 = mock(ApplicationCommandInteractionOptionData.class);
+        ApplicationCommandInteractionOption parameter2 = new ApplicationCommandInteractionOption(
+                gateway,
+                data2,
+                1L,
+                mock(ApplicationCommandInteractionResolvedData.class)
+        );
+        when(event.getOption(eq("name"))).thenReturn(Optional.of(parameter));
+        when(event.getOption(eq("url"))).thenReturn(Optional.of(parameter2));
+        when(data.value()).thenReturn(Possible.of("Character"));
+        when(data.type()).thenReturn(3);
+        when(data2.value()).thenReturn(Possible.of("url0/url1/url2/url3/url4/url5"));
+        when(data2.type()).thenReturn(3);
+
+        Message messageResponse = new Message(gateway, MessageData.builder()
+                .applicationId(1L)
+                .id(1L)
+                .channelId(200L)
+                .type(ApplicationCommandOption.Type.STRING.ordinal())
+                .author(UserData.builder().id(1L).username("user").discriminator("#0").build())
+                .components(ComponentData.builder().type(MessageComponent.Type.BUTTON.ordinal()).build())
+                .content("Character sheet added")
+                .timestamp("today")
+                .tts(false)
+                .mentionEveryone(false)
+                .pinned(false)
+                .build());
+        when(event.createFollowup(any(InteractionFollowupCreateSpec.class)))
+                .thenReturn(Mono.just(messageResponse));
+        when(characterSheetRepository.save(any(CharacterSheet.class))).thenReturn(characterSheet);
+        when(event.createFollowup(anyString())).thenReturn(InteractionFollowupCreateMono.of(event).withContent(messageResponse.getContent()));
+        Mono<Message> response = characterSheetService.addCharacter(event);
+
+        StepVerifier.create(response)
+                .expectNextMatches(message -> message.getContent().equals(messageResponse.getContent()))
+                .verifyComplete();
     }
 
     @Test
     void updateCharacter() {
+        ApplicationCommandInteractionOptionData data2 = mock(ApplicationCommandInteractionOptionData.class);
+        ApplicationCommandInteractionOption parameter2 = new ApplicationCommandInteractionOption(
+                gateway,
+                data2,
+                1L,
+                mock(ApplicationCommandInteractionResolvedData.class)
+        );
+        when(event.getOption(eq("name"))).thenReturn(Optional.of(parameter));
+        when(event.getOption(eq("owner-id"))).thenReturn(Optional.of(parameter2));
+        when(data.value()).thenReturn(Possible.of("Character"));
+        when(data.type()).thenReturn(3);
+        when(data2.value()).thenReturn(Possible.of("1"));
+        when(data2.type()).thenReturn(3);
+
+        Message messageResponse = new Message(gateway, MessageData.builder()
+                .applicationId(1L)
+                .id(1L)
+                .channelId(200L)
+                .type(ApplicationCommandOption.Type.STRING.ordinal())
+                .author(UserData.builder().id(1L).username("user").discriminator("#0").build())
+                .components(ComponentData.builder().type(MessageComponent.Type.BUTTON.ordinal()).build())
+                .content("Character **Character** has been updated with ownerId 1.")
+                .timestamp("today")
+                .tts(false)
+                .mentionEveryone(false)
+                .pinned(false)
+                .build());
+        when(event.createFollowup(any(InteractionFollowupCreateSpec.class)))
+                .thenReturn(Mono.just(messageResponse));
+        when(characterSheetRepository.findByName(anyString())).thenReturn(characterSheet);
+        when(characterSheetRepository.save(any(CharacterSheet.class))).thenReturn(characterSheet);
+        when(event.createFollowup(anyString())).thenReturn(InteractionFollowupCreateMono.of(event).withContent(messageResponse.getContent()));
+
+        Mono<Message> response = characterSheetService.updateCharacter(event);
+
+        StepVerifier.create(response)
+                .expectNextMatches(message -> message.getContent().equals(messageResponse.getContent()))
+                .verifyComplete();
     }
 
     @Test
     void removeCharacter() {
+        when(event.getOption(eq("name"))).thenReturn(Optional.of(parameter));
+        when(data.value()).thenReturn(Possible.of("Character"));
+        when(data.type()).thenReturn(3);
+
+        Message messageResponse = new Message(gateway, MessageData.builder()
+                .applicationId(1L)
+                .id(1L)
+                .channelId(200L)
+                .type(ApplicationCommandOption.Type.STRING.ordinal())
+                .author(UserData.builder().id(1L).username("user").discriminator("#0").build())
+                .components(ComponentData.builder().type(MessageComponent.Type.BUTTON.ordinal()).build())
+                .content("Character sheet deleted")
+                .timestamp("today")
+                .tts(false)
+                .mentionEveryone(false)
+                .pinned(false)
+                .build());
+        when(event.createFollowup(any(InteractionFollowupCreateSpec.class)))
+                .thenReturn(Mono.just(messageResponse));
+        when(characterSheetRepository.findByNameAndOwnerId("Character", "1")).thenReturn(characterSheet);
+        doNothing().when(characterSheetRepository).delete(characterSheet);
+        when(event.createFollowup(anyString())).thenReturn(InteractionFollowupCreateMono.of(event).withContent(messageResponse.getContent()));
+
+        Mono<Message> response = characterSheetService.removeCharacter(event);
+
+        StepVerifier.create(response)
+                .expectNextMatches(message -> message.getContent().equals(messageResponse.getContent()))
+                .verifyComplete();
     }
 
     @Test
-    void readSheetValue() {
+    void readSheetValue() throws GeneralSecurityException, IOException {
+        ApplicationCommandInteractionOptionData data2 = mock(ApplicationCommandInteractionOptionData.class);
+        ApplicationCommandInteractionOption parameter2 = new ApplicationCommandInteractionOption(
+                gateway,
+                data2,
+                1L,
+                mock(ApplicationCommandInteractionResolvedData.class)
+        );
+        when(event.getOption(eq("name"))).thenReturn(Optional.of(parameter));
+        when(event.getOption(eq("value"))).thenReturn(Optional.of(parameter2));
+        when(data.value()).thenReturn(Possible.of("Character"));
+        when(data.type()).thenReturn(3);
+        when(data2.value()).thenReturn(Possible.of("Spell"));
+        when(data2.type()).thenReturn(3);
+        BatchGetValuesResponse sheetMatrix = mock(BatchGetValuesResponse.class);
+        ValueRange spells = mock(ValueRange.class);
+        when(spells.getValues()).thenReturn(List.of(List.of("Spell")));
+        ValueRange descriptions = mock(ValueRange.class);
+        when(descriptions.getValues()).thenReturn(List.of(List.of("Description")));
+        when(sheetMatrix.getValueRanges()).thenReturn(List.of(spells, descriptions));
+
+        Message messageResponse = new Message(gateway, MessageData.builder()
+                .applicationId(1L)
+                .id(1L)
+                .channelId(200L)
+                .type(ApplicationCommandOption.Type.STRING.ordinal())
+                .author(UserData.builder().id(1L).username("user").discriminator("#0").build())
+                .components(ComponentData.builder().type(MessageComponent.Type.BUTTON.ordinal()).build())
+                .content("Spell's **Description** is: Description")
+                .timestamp("today")
+                .tts(false)
+                .mentionEveryone(false)
+                .pinned(false)
+                .build());
+        when(event.createFollowup(any(InteractionFollowupCreateSpec.class)))
+                .thenReturn(Mono.just(messageResponse));
+        when(characterSheetRepository.findByNameAndOwnerId("Character", "1")).thenReturn(characterSheet);
+        when(characterSheetDetailsService.getSpreadsheetMatrix(any(CharacterSheet.class), anyList())).thenReturn(sheetMatrix);
+        when(event.createFollowup(anyString())).thenReturn(InteractionFollowupCreateMono.of(event).withContent(messageResponse.getContent()));
+
+        Mono<Message> response = characterSheetService.readSheetValue(event);
+
+        StepVerifier.create(response)
+                .expectNextMatches(message -> message.getContent().equals(messageResponse.getContent()))
+                .verifyComplete();
     }
 }
