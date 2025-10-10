90692143347a6187ad54754a9de4460ca2c95f04
ciji <cijik1@gmail.com>
2025-10-10 14:42:41 +0300

    Add support for health checks and app stability

--- src/main/java/com/ciji/serenity/config/Client.java
+++ src/main/java/com/ciji/serenity/config/Client.java
@@ -2,10 +2,16 @@ package com.ciji.serenity.config;
 
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.presence.ClientActivity;
+import discord4j.core.object.presence.ClientPresence;
+import jakarta.annotation.PreDestroy;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 @Slf4j
@@ -22,12 +28,32 @@ public class Client {
         try {
             client = DiscordClientBuilder.create(token)
                     .build()
+                    .gateway()
+                    .setInitialPresence(s -> ClientPresence.online(ClientActivity.playing("Starting...")))
                     .login()
+                    .retryWhen(Retry.backoff(5, Duration.ofSeconds(5))
+                            .maxBackoff(Duration.ofMinutes(2))
+                            .doBeforeRetry(signal ->
+                                    log.warn("Retrying connection to Discord. Attempt: {}", signal.totalRetries() + 1)))
+
                     .block();
         } catch (NullPointerException e) {
             log.error("Be sure to use a valid bot token!", e);
         }
 
+        if (client != null) {
+            client.onDisconnect()
+                    .doOnTerminate(() -> log.warn("Disconnected from Discord Gateway"))
+                    .subscribe();
+        }
+
         this.client = client;
+    }
+
+    @PreDestroy
+    public void onShutdown() {
+        if (client != null) {
+            client.logout().block();
+        }
     }
 }
